49b1db94b922e139d6a56b177a21e44b9440560e
Luigi Maciel <luigimaciel@Luigis-MacBook-Air.local>
2025-07-01 15:59:16 -0300

    Corrige vite.config.js para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/' // <-- nome EXATO do repositório no GitHub
+  // ⚠️ Para Vercel NÃO USE base: './' ou base: '/app/'
 })
