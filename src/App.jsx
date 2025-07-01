602377cdca78a4290f0834e1d330e5c0a3bffb98
Luigi Maciel <luigimaciel@Luigis-MacBook-Air.local>
2025-07-01 15:54:10 -0300

    Atualiza vite e package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import Funcionario from './Funcionario'
 import Admin from './Admin'
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/funcionario" element={<Funcionario />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/funcionario" element={<Funcionario />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/" element={<Login />} />
+    </Routes>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/app/' // <-- ESSENCIAL para GitHub Pages
+  base: '/app/' // <-- nome EXATO do repositório no GitHub
 })
