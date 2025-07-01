3dac22fb56fc50ceeaa7a5938dd6efbb201e5fe1
Luigi Maciel <luigimaciel@Luigis-MacBook-Air.local>
2025-07-01 14:16:29 -0300

    build: script gerado corretamente pelo Vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html') // 👈 isso força o Vite a incluir o index.html certo
+      }
+    }
+  }
 })
